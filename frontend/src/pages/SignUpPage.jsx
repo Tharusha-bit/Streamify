e7107c6145bab5f03836845bb22ca289dea37500
Tharusha-bit <tharushanimnathme@gmail.com>
2025-10-08 22:05:52 +0530

    layout section
    layout section finished (Theme selector yet to be)

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { ShipWheelIcon } from "lucide-react";
 import { Link } from "react-router";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { signup } from "../lib/api";
 import useSignup from "../hooks/useSignup";
 
 const SignUpPage = () => {
